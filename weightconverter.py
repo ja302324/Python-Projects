48eed18bcec05944dd2008803e3e3800cab24c43
Jesse Adamu <jesseadamu2021@gmail.com>
2024-01-30 22:33:42 -0500

    Python weight converter project

---
+++ weightconverter.py
@@ -0,0 +1,12 @@
+weight = input("Weight: ")
+
+what_weight = input("(L)bs or (K)g: ")
+
+if what_weight.lower() == "l":
+    weight = float(weight) * 0.4536
+elif what_weight.lower() == "k":
+    weight = float(weight) / 0.4536
+else:
+    print("Wrong Charater!")
+
+print(f"You're weight is {weight}")
